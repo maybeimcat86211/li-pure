9b720c42d33501b3aa83531dc4d3f7f1969fcf25
maybeimcat86211 <hank86211@gmail.com>
2022-08-15 17:07:58 +0800

    更新資料庫

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)  #位置名稱
+
+class Post(models.Model):
+    subject = models.CharField(max_length=255)  #標題
+    content = models.CharField(max_length=255)  #內容
+    author = models.CharField(max_length=20)  #貼文者
+    publish_date = models.DateField(default=timezone.now)  #貼文時間
+    location = models.ForeignKey(Location, on_delete=models.CASCADE) #位置，CASCADE連帶刪除
