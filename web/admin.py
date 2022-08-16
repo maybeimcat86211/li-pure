fad4133f826b8c3322fbc19e344b48133512a593
maybeimcat86211 <hank86211@gmail.com>
2022-08-16 12:09:21 +0800

    admin.py update

--- web/admin.py
+++ web/admin.py
@@ -4,6 +4,8 @@ from .models import Location, Post
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'content', 'author', 'location')
 
 admin.site.register(Location)  #註冊至Administration(管理員後台)
-admin.site.register(Post)  #註冊至Administration(管理員後台)
+admin.site.register(PostAdmin)  #註冊至Administration(管理員後台)
