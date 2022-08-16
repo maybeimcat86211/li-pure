23ca97767c5b5fb22dc87b0d15d408e66300ab64
maybeimcat86211 <hank86211@gmail.com>
2022-08-16 11:34:20 +0800

    更新admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Location, Post
 # Register your models here.
+
+admin.site.register(Location)  #註冊至Administration(管理員後台)
+admin.site.register(Post)  #註冊至Administration(管理員後台)
